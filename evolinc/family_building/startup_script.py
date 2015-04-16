eb1fa7007d81c456d85d17f47f8be23266782ff4
Asher <asherkhb@gmail.com>
2015-04-16 13:08:11 -0700

    Startup Script, v.2.0.1

--- evolinc/family_building/startup_script.py
+++ evolinc/family_building/startup_script.py
@@ -1,35 +1,43 @@
 __author__ = 'asherkhb'
 
 from subprocess import call
-import sys
+from sys import argv
 
 #Ensure correct number of arguments are entered
-if len(sys.argv) != 2:
+if len(argv) != 2:
     print("Incorrect usage syntax: Must specify input list.\nUse 'startup_script.py <input>'")
     exit()
 
 #Assign input file from first command argument.
-inputfile = sys.argv[1]
+inputfile = argv[1]
 
 with open(inputfile, 'r') as inpt:
-    reading = True
-    while reading:
-        line = inpt.readline()
-        if line =='':
-            reading = False
-        elif line == '\n':
+    for line in inpt:
+        if line == '\n':
             pass
         else:
             line_split = line.split('\t')
+
             subject = line_split[0]
             lincRNA = line_split[1]
             query_spp = line_split[2]
             subject_spp = line_split[3]
-            if len(line_split) == 5:
-                subject_gff = line_split[4]
-                query = "sh Family_Building.sh -g %s -s %s -q %s -f %s" % \
-                        (subject, lincRNA, query_spp, subject_spp, subject_gff)
-            else:
-                query = "sh Family_Building.sh -g %s -s %s -q %s -f %s" % \
+            try:
+                try:
+                    subject_gff = line_split[4]
+                    known_lincRNAs = line_split[5]
+                    if subject_gff != '':
+                        query = "sh Family_Building.sh -g %s -l %s -q %s -s %s -e %s -k %s" % \
+                                (subject, lincRNA, query_spp, subject_spp, subject_gff, known_lincRNAs)
+                    else:
+                        query = 'sh Building_Families_no_GFF.sh -g %s -l %s -q %s -s %s -k %s' % \
+                                (subject, lincRNA, query_spp, subject_spp, known_lincRNAs)
+
+                except IndexError:
+                    subject_gff = line_split[4]
+                    query = 'sh Building_Families_no_lincRNA_database.sh -g %s -l %s -q %s -s %s -e %s' % \
+                            (subject, lincRNA, query_spp, subject_spp, subject_gff)
+            except IndexError:
+                query = 'sh Building_families_no_GFF_no_lincRNA_database.sh -g %s -l %s -q %s -s %s' % \
                         (subject, lincRNA, query_spp, subject_spp)
-            call(query, shell=True)
+        call(query, shell=True)
